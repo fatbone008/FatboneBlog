8491378df3936d958bc523ec29b36d883ded229c
Willian Chan <165080496@qq.com>
2017-04-18 10:52:27 +0800

    reiterate the articles in index

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var Post = require('./../libs/mongoose').Post
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    console.log('request router:\'/\'');
-    res.render('index', {title: 'Blog of Fatbone'});
+    Post.find({})
+        .select("title create_at")
+        .exec(function (err, articles) {
+            if(err) res.send("Something wrong accured when query the article list.")
+            res.render('index',{
+                title:"Fat-Bone Blog",
+                articles:articles
+            })
+        });
 });
 
 router.use('/backend', require('./backend/index'));
