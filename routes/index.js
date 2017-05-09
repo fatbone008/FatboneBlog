061c189f0d0f1e983028646ead6f99ea4fd5618e
Willian Chan <165080496@qq.com>
2017-05-09 09:41:18 +0800

    index articles css styles

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,28 @@ var Post = require('./../libs/mongoose').Post
 /* GET home page. */
 router.get('/', function (req, res, next) {
     Post.find({})
-        .select("title create_at")
+        .select("title create_at _id")
         .exec(function (err, articles) {
-            if(err) res.send("Something wrong accured when query the article list.")
+            if(err) res.send("Something wrong accured when query the article list.");
             res.render('index',{
                 title:"Fat-Bone Blog",
-                articles:articles
+                articles:articles,
+                subtitle:"A lovely fat programmer who pursuing full stack programming."
             })
         });
 });
+
+router.get('/article',function (req,res,next) {
+    Post.findOne({_id:req.query._id})
+        .exec(function (err, article) {
+            if(err) res.render('404');
+            res.render('frontend/article',{
+                title:article.title,
+                article:article,
+                subtitle:!!article.subtitle || article.title
+            });
+        });
+})
 
 router.use('/backend', require('./backend/index'));
 
